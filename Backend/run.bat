21e4914f87ebd96aa38e77d7df23e86f01ed0270
GIHAN SERASINGHE <99310585+Gihandev@users.noreply.github.com>
2025-11-23 15:48:23 +0530

    Delete Backend/run.bat

--- Backend/run.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Activate the virtual environment
-call venv\Scripts\activate
-
-REM Run the Flask app
-python run.py
-
-REM Deactivate the virtual environment after running the app
-deactivate
